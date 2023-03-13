58167a699d30abcc6f6988d3b621b3f77ea85f11
Arjun Poudel <arjunpoudel703@gmail.com>
2023-03-13 08:43:02 +0545

    ..

--- itsvidtime-php-part/db.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-$host = 'db4free.net';
-$user = 'itsvidtime';
-$pass = 'Fawcdtgk789!';
-$db = 'itsvidtime';
-
- ?>
--- itsvidtime-php-part/globalvars.js
+++
@@ -1,4 +0,0 @@
-var userDetails={};
-var socketURL = "http://localhost:3000";
-
-//var socketURL = "https://itsvidtimen.herokuapp.com/";
